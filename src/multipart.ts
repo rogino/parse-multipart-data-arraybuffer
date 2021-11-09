777eddb412f0537fe83a9ba2ce9b04e2b9c8f784
IgorHrabrov <18480638+IgorHrabrov@users.noreply.github.com>
2021-11-09 18:31:22 +0200

    Removed wrapping quotes from boundary

--- src/multipart.ts
+++ src/multipart.ts
@@ -96,7 +96,7 @@ export function getBoundary(header: string): string {
       const item = new String(items[i]).trim()
       if (item.indexOf('boundary') >= 0) {
         const k = item.split('=')
-        return new String(k[1]).trim()
+        return new String(k[1]).trim().replace(/^["']|["']$/g, "")
       }
     }
   }
